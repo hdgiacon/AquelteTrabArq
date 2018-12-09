4a3832119a59987f24516ad1131b0ea9dab559fc
Fernando Silvério <ssilveriofernando45@gmail.com>
2018-12-09 15:47:46 -0200

    ok

---
+++ src/Codigos/Memoria.java
@@ -0,0 +1,52 @@
+package Codigos;
+
+public class Memoria {
+    private Instrução i;
+    private Registrador param1;
+    private Registrador param2;
+    private Registrador param3;
+    private Integer paramInt;
+
+    public Instrução getI() {
+        return i;
+    }
+
+    public void setI(Instrução i) {
+        this.i = i;
+    }
+
+    public Registrador getParam1() {
+        return param1;
+    }
+
+    public void setParam1(Registrador param1) {
+        this.param1 = param1;
+    }
+
+    public Registrador getParam2() {
+        return param2;
+    }
+
+    public void setParam2(Registrador param2) {
+        this.param2 = param2;
+    }
+
+    public Registrador getParam3() {
+        return param3;
+    }
+
+    public void setParam3(Registrador param3) {
+        this.param3 = param3;
+    }
+
+    public Integer getParamInt() {
+        return paramInt;
+    }
+
+    public void setParamInt(Integer paramInt) {
+        this.paramInt = paramInt;
+    }
+    
+    
+    
+}
